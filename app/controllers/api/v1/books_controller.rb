346cb0fbaa5c1bd0adb33115ca630a860406b023
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-07-19 14:42:56 -0700

    added create method in bookcontroller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -16,5 +16,23 @@ module Api::V1
       )
     end
 
+    def create
+      @book = Book.new(book_params)
+      if @book.save
+        render(
+          status: :ok,
+          json: @book.as_json(
+            only: [:title, :author, :image_url, :status]
+          )
+        )
+      end
+    end
+
+    private
+
+    def book_params
+      return params.require(:book).permit(:status, :title, :author)
+    end
   end
+
 end
