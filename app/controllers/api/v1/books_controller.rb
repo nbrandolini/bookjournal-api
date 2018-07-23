81df5465a6af285f20cbe5776dc7ed6d910423b0
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-07-23 16:42:17 -0700

    not sure

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -3,8 +3,9 @@ module Api::V1
   class BooksController < ApplicationController
 
     def index
-      @books = Book.all
+      @books= Book.all
       render json: @books
+      @read = Book.all.where(status:"read")
     end
 
     def show
